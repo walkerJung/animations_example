ba572c9314208b54a1e0aad287140683d0439a30
걷는사람 정정훈 <92363576+walkerJung@users.noreply.github.com>
2022-02-07 22:35:19 +0900

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Animated } from "react-native";
+import styled from "styled-components/native";
+
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+const Box = styled.TouchableOpacity`
+  background-color: tomato;
+  width: 200px;
+  height: 200px;
+`;
+const AnimatedBox = Animated.createAnimatedComponent(Box);
 
 export default function App() {
+  const Y = new Animated.Value(0);
+  const moveUp = () => {};
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Container>
+      <AnimatedBox
+        onPress={moveUp}
+        style={{
+          transform: [{ translateY: Y }],
+        }}
+      />
+    </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
